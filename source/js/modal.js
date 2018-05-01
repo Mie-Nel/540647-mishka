2f92c40515b6e81ea98d5afaf1653e744e7dd5b2
Mie-Nel <mielikki@mail.ru>
2018-05-01 18:19:53 +0300

    add index, form, catalog

---
+++ source/js/modal.js
@@ -0,0 +1,23 @@
+var modal = document.querySelector(".modal");
+var orderBtn = document.querySelector(".product__link");
+var modalClose = document.querySelector(".modal__overlay");
+
+orderBtn.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal--closed");
+  modal.classList.add("modal--opened");
+});
+
+modalClose.addEventListener("click", function () {
+  modal.classList.remove("modal--opened");
+  modal.classList.add("modal--closed");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains("modal--opened")) {
+      modal.classList.remove("modal--opened");
+      modal.classList.add("modal--closed");
+    }
+  }
+});
